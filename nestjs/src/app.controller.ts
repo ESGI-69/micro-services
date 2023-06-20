0942b8e3fbeaf26fba226b9bf85ca6c43e747e37
timdev0 <quentin.peltier.pro@gmail.com>
2023-06-20 12:00:16 +0200

    prisma & controlller
    Co-authored-by: Gatien Anizan <gatien.anizan@gmail.com>

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,12 +1,67 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import {
+  AddRequest,
+  AddResponse,
+  DeleteRequest,
+  DeleteResponse,
+  GetRequest,
+  GetResponse,
+  HERO_CR_UD_SERVICE_NAME,
+  Hero,
+  HeroCRUDServiceController,
+  UpdateRequest,
+  UpdateResponse,
+  HeroCRUDServiceControllerMethods,
+} from './stubs/hero/v1alpha/hero';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata } from '@grpc/grpc-js';
 @Controller()
-export class AppController {
+@HeroCRUDServiceControllerMethods()
+export class AppController implements HeroCRUDServiceController {
   constructor(private readonly appService: AppService) {}
+  async get(
+    request: GetRequest,
+    //metadata?: Metadata
+  ): Promise<GetResponse> {
+    let hero: Hero;
+    let heroes: Hero[] = [];
+    if (request.id) {
+      hero = await this.appService.findById(request.id);
+      return { heroes: [hero] };
+    } else if (request.name) {
+      hero = await this.appService.findByName(request.name);
+      return { heroes: [hero] };
+    } else {
+      heroes = await this.appService.findAll();
+      return { heroes };
+    }
+  }
+  async update(
+    request: UpdateRequest,
+    //metadata?: Metadata,
+  ): Promise<UpdateResponse> {
+    const hero = await this.appService.update(request.id, {
+      name: request.name,
+      power: request.power,
+      hp: request.hp,
+    });
+    return { hero };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async delete(
+    request: DeleteRequest,
+    // metadata?: Metadata,
+  ): Promise<DeleteResponse> {
+    const hero = await this.appService.delete(request.id);
+    return { hero };
+  }
+  async add(request: AddRequest): Promise<AddResponse> {
+    const hero = await this.appService.create({
+      name: request.name,
+      power: request.power,
+      hp: 100,
+    });
+    return { hero };
   }
 }
--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Hero } from './stubs/hero/v1alpha/hero';
+import { PrismaService } from './prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+  create(data: Prisma.HeroCreateInput): Promise<Hero> {
+    return this.prisma.hero.create({ data });
+  }
+  findAll(): Promise<Hero[]> {
+    return this.prisma.hero.findMany();
+  }
+  findById(id: number): Promise<Hero> {
+    return this.prisma.hero.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  findByName(name: string): Promise<Hero> {
+    return this.prisma.hero.findFirst({
+      where: { name },
+    });
+  }
+
+  async update(id: number, data: Prisma.HeroUpdateInput): Promise<Hero> {
+    return this.prisma.hero.update({
+      where: { id },
+      data,
+    });
+  }
+
+  delete(id: number): Promise<Hero> {
+    return this.prisma.hero.delete({
+      where: { id },
+    });
   }
 }
