04fce7db98e02a8dd32afdb5ad1c57ca2b6a264f
timdev0 <quentin.peltier.pro@gmail.com>
2023-07-04 16:55:40 +0200

    nestconf & santé : 1st exercice done
    Co-authored-by: Gatien Anizan <gatien.anizan@gmail.com>

--- nestjs/src/user/user.module.ts
+++ nestjs/src/user/user.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ClientsModule } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { userGrpcOptions } from 'src/grpc.config';
 @Module({
-  imports: [ClientsModule.register([userGrpcOptions])],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.registerAsync([
+      {
+        name: 'USER_PACKAGE',
+        imports: [ConfigModule],
+        useFactory: (cs: ConfigService) => userGrpcOptions(cs),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   providers: [UserService],
   exports: [UserService],
 })
--- nestjs/src/user/user.service.ts
+++ nestjs/src/user/user.service.ts
@@ -2,19 +2,20 @@ import { OnModuleInit } from '@nestjs/common';
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { FindRequest, FindResponse, User } from '../stubs/user/v1alpha/message';
-import {
-  USER_SERVICE_NAME,
-  UserServiceClient,
-} from '../stubs/user/v1alpha/service';
+import { UserServiceClient } from '../stubs/user/v1alpha/service';
 import { firstValueFrom } from 'rxjs';
 import { Metadata } from '@grpc/grpc-js';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class UserService implements OnModuleInit {
   private userService: UserServiceClient;
-  constructor(@Inject(USER_SERVICE_NAME) private client: ClientGrpc) {}
+  constructor(
+    @Inject('USER_PACKAGE') private client: ClientGrpc,
+    private configService: ConfigService,
+  ) {}
   onModuleInit() {
-    this.userService =
-      this.client.getService<UserServiceClient>(USER_SERVICE_NAME);
+    this.userService = this.client.getService<UserServiceClient>('UserService');
   }
   async findUser(req: FindRequest, md: Record<string, any>): Promise<User> {
     const meta = new Metadata();
